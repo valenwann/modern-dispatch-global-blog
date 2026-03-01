e8a4805c3df1743e2a333cee6acb380e906769eb
valenwann <valenwanji93@gmail.com>
2026-03-02 01:48:00 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,9 @@
-import '../styles/globals.css'
-import Layout from '../components/Layout'
+import Layout from "../components/Layout";
 
-export default function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
-  )
+  );
 }
