efd06844e21331c4c18f9e87f8e9523db0d95624
valenwann <valenwanji93@gmail.com>
2026-03-01 22:29:22 +0300

    Create privacy.js

---
+++ pages/privacy.js
@@ -0,0 +1,26 @@
+export default function Privacy() {
+  return (
+    <main className="container">
+
+      <h1>Privacy Policy</h1>
+
+      <p>
+        Modern Dispatch Global respects your privacy.
+      </p>
+
+      <p>
+        We collect minimal information necessary to improve user experience,
+        analytics, and newsletter communication.
+      </p>
+
+      <p>
+        We do not sell personal data.
+      </p>
+
+      <p>
+        Users may unsubscribe from communications at any time.
+      </p>
+
+    </main>
+  )
+}
