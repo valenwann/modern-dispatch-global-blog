6ef66972b3a0afaee0df4d20020cbd406e6eeff2
valenwann <valenwanji93@gmail.com>
2026-03-01 22:35:08 +0300

    Delete components directory

--- components/Footer.js
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-  return (
-    <div className="footer container">
-      <p>©2024cS Modern Dispatch Global</p>
-      <p>Luxury global intelligence and industrial analysis.</p>
-    </div>
-  )
-    }
