bb407814472793854a970491de4c93f98a00c4a8
valenwann <valenwanji93@gmail.com>
2026-03-02 01:46:17 +0300

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,27 @@
+import Header from "./Header";
+
 export default function Layout({ children }) {
   return (
     <>
-      {children}
+      <Header />
+
+      <main style={{
+        maxWidth: "900px",
+        margin: "0 auto",
+        padding: "30px 20px"
+      }}>
+        {children}
+      </main>
+
+      <footer style={{
+        textAlign: "center",
+        padding: "40px",
+        marginTop: "60px",
+        borderTop: "1px solid #eee"
+      }}>
+        © 2024c THE MODERN DISPATCH GLOBAL
+      </footer>
+
     </>
   );
 }
