c3e84f1775e3bff0e2e53e41239e09639fa64423
valenwann <valenwanji93@gmail.com>
2026-03-01 22:56:40 +0300

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer style={{
+      marginTop:40,
+      padding:20,
+      borderTop:"1px solid #ddd",
+      textAlign:"center"
+    }}>
+      © 2024c Modern Dispatch Global
+    </footer>
+  )
+}
