f7685f9b7a65d37dafb59f12d66bfcde109a25cf
valenwann <valenwanji93@gmail.com>
2026-03-02 11:47:40 +0300

    Update ArticleCard.js

--- components/ArticleCard.js
+++ components/ArticleCard.js
@@ -1,50 +1,52 @@
 ```javascript
-export default function ArticleCard({ title, image, category, date }) {
+export default function ArticleCard({ title, category, date, image }) {
   return (
-    <div style={{
-      background: "#111",
+    <article style={{
+      background: "#111111",
       borderRadius: "12px",
       overflow: "hidden",
-      marginBottom: "20px",
-      cursor: "pointer"
+      marginBottom: "24px",
+      border: "1px solid #222"
     }}>
-
+      
       <img
         src={image}
+        alt={title}
         style={{
           width: "100%",
-          height: "200px",
+          height: "220px",
           objectFit: "cover"
         }}
       />
 
-      <div style={{ padding: "15px" }}>
-
+      <div style={{ padding: "16px" }}>
+        
         <div style={{
           color: "#FF6A00",
           fontSize: "12px",
-          marginBottom: "5px"
+          marginBottom: "6px"
         }}>
           {category}
         </div>
 
         <h2 style={{
-          color: "white",
-          fontSize: "18px",
-          margin: "0 0 5px 0"
+          color: "#fff",
+          fontSize: "20px",
+          margin: "0 0 8px 0"
         }}>
           {title}
         </h2>
 
         <div style={{
-          color: "#A1A1AA",
-          fontSize: "12px"
+          color: "#888",
+          fontSize: "13px"
         }}>
           {date}
         </div>
 
       </div>
-    </div>
+
+    </article>
   );
 }
 ```
